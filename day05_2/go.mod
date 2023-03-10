4f35c26c615c08de45a9e38294dbb5d730106f06
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon-2.local>
2023-03-10 20:11:57 +0100

    Add solution for day 5 part 2

---
+++ day05_2/go.mod
@@ -0,0 +1,3 @@
+module day05_2
+
+go 1.19
---
+++ day05_2/supply.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type stack []int
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	scanner := bufio.NewScanner(file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+func main() {
+	m := make(map[int][]string)
+	lines, _ := readLines("input.txt")
+
+	for i, l := range lines {
+		if i < 8 {
+			counter := 0
+			for j := 1; j <= len(l); j += 4 {
+				if string(l[j]) != " " {
+					m[counter] = append(m[counter], string(l[j]))
+				}
+				counter++
+			}
+		}
+
+		if i > 9 {
+			s := strings.Split(l, " ")
+
+			times, _ := strconv.Atoi(s[1])
+			from, _ := strconv.Atoi(s[3])
+			to, _ := strconv.Atoi(s[5])
+
+			popped := m[from-1][:times]
+			m[from-1] = m[from-1][times:]
+
+			for k := len(popped); k >= 1; k-- {
+				m[to-1] = append([]string{popped[k-1]}, m[to-1]...)
+			}
+		}
+	}
+
+	fmt.Print(m)
+}
